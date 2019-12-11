c44bc28e2db4a73b49ef31a681785a186e8d2344
Jukka Edvardsson <1426249+ejukka@users.noreply.github.com>
2019-12-11 22:38:13 +0200

    Added missing alt to images

--- src/pages/apartments/apartments.page.tsx
+++ src/pages/apartments/apartments.page.tsx
@@ -14,15 +14,18 @@ const ApartmentsPage = () => (
       interval={2000}
     >
       <div>
-        <img src="https://i.postimg.cc/HsF3Mxsd/image1.jpg" />
+        <img
+          src="https://i.postimg.cc/HsF3Mxsd/image1.jpg"
+          alt={"house b2 and b3"}
+        />
         <p className="legend">Paritalo: asunnot B2 ja B3</p>
       </div>
       <div>
-        <img src="https://i.postimg.cc/158J46Sv/image2.jpg" />
+        <img src="https://i.postimg.cc/158J46Sv/image2.jpg" alt={"house a"} />
         <p className="legend">Erillistalo: Asunto A</p>
       </div>
       <div>
-        <img src="https://i.postimg.cc/YqCTRhHF/image3.jpg" />
+        <img src="https://i.postimg.cc/YqCTRhHF/image3.jpg" alt={"house a"} />
         <p className="legend">Erillistalo: Asunto A</p>
       </div>
     </Carousel>
