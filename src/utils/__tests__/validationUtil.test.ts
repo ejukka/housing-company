524f31348e08baf02c3b9c2308d99a6a4f6fa844
Jukka Edvardsson <1426249+ejukka@users.noreply.github.com>
2020-01-05 23:18:41 +0200

    Changed test to ts-file

---
+++ src/utils/__tests__/validationUtil.test.ts
@@ -0,0 +1,14 @@
+import validateUser from "../validationUtil";
+
+describe("test validationUtil", () => {
+  describe("test validateUser", () => {
+    test("validateUser should return valid user", () => {
+      const validatedUser = validateUser("test");
+      expect(validatedUser).toEqual(true);
+    });
+    test("validateUser should return invalid user", () => {
+      const validatedUser = validateUser("test");
+      expect(validatedUser).toEqual(false);
+    });
+  });
+});
