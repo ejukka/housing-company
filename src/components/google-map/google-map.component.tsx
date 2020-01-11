5d630dc1505b40912534a158a85f15996a7c0a3f
Jukka Edvardsson <1426249+ejukka@users.noreply.github.com>
2020-01-11 23:01:02 +0200

    Updated outdated dependencies. Also changed latitude and longitude to the env file

--- src/components/google-map/google-map.component.tsx
+++ src/components/google-map/google-map.component.tsx
@@ -2,9 +2,17 @@ import React from "react";
 
 import { withGoogleMap, GoogleMap } from "react-google-maps";
 
+const config = {
+  latitude: `${process.env.REACT_APP_LATITUDE}`,
+  longitude: `${process.env.REACT_APP_LONGITUDE}`
+};
+
 const GoogleMapResult = withGoogleMap(props => (
   <GoogleMap
-    defaultCenter={{ lat: 60.3624643, lng: 24.3637589 }}
+    defaultCenter={{
+      lat: parseInt(config.latitude),
+      lng: parseInt(config.longitude)
+    }}
     defaultZoom={13}
   />
 ));
