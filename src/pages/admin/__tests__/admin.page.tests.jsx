b9904400c652e76609948fdd67f620b37b9c0535
Jukka Edvardsson <1426249+ejukka@users.noreply.github.com>
2020-01-14 23:23:02 +0200

    Added snapshot test for admin page

--- src/pages/admin/__tests__/admin.page.tests.jsx
+++ src/pages/admin/__tests__/admin.page.tests.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
-import AdminPage from '../admin.pagee';
-import renderer from 'react-test-renderer';
+import AdminPage from '../admin.page';
+import reduxStore from "../../../redux/store";
+import {act} from "react-dom/test-utils";
+import ReactDOM from "react-dom";
+import {Provider} from "react-redux";
+import pretty from "pretty";
+
+let container;
+
+beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    document.body.removeChild(container);
+    container = null;
+});
 
 test('Renders Admin page', () => {
-    const component = renderer.create(
-        <AdminPage />
-    );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    // const component = renderer.create(
+    //     <AdminPage />
+    // );
+    // let tree = component.toJSON();
+    // expect(tree).toMatchSnapshot();
+
+    const store = reduxStore.store;
+    act(() => {
+        ReactDOM.render(
+            <Provider store={store}>
+                <AdminPage />
+            </Provider>,
+            container
+        );
+    });
+    expect(pretty(container.innerHTML)).toMatchSnapshot();
 });
