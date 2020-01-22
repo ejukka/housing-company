9578dd6e86e80230c9ba809bff689b57f2163046
Jukka Edvardsson <1426249+ejukka@users.noreply.github.com>
2020-01-22 23:30:44 +0200

    Updated typescript version to 3.7.5. Also changed History type back to any, because I didn't get it working properly.

--- src/types/MenuProps.ts
+++ src/types/MenuProps.ts
@@ -1,10 +1,8 @@
-import { History, LocationState } from "history";
-
 interface menuProps {
   title: string;
   imageUrl: string;
   size: string;
-  history: History<LocationState>;
+  history: any;
 }
 
 export default menuProps;
