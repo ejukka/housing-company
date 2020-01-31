06ec21f297cec58bd6e7f4467e9bdf5165fa0222
Jukka Edvardsson <1426249+ejukka@users.noreply.github.com>
2020-01-31 23:12:16 +0200

    Updated about pages tests

--- src/pages/about/__tests__/about.page.test.js
+++ src/pages/about/__tests__/about.page.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import AboutPage from '../about.page';
 import renderer from 'react-test-renderer';
+import {aboutPageConfig} from "../../../utils/config";
 
 test('Renders About page', () => {
     const component = renderer.create(
-        <AboutPage />
+        <AboutPage props={aboutPageConfig}/>
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
