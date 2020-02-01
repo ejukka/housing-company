c7b738f32405056c94a51e4f69795ef5d5745a99
Jukka Edvardsson <1426249+ejukka@users.noreply.github.com>
2020-02-01 23:28:21 +0200

    Added simple tests that all pages are rendered correctly

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,5 +1,14 @@
 describe('Home Page test', function() {
     it('successfully loads home page', function() {
         cy.visit('/');
-    })
+    });
+    it('successfully loads aparments page', function() {
+        cy.visit('/apartments');
+    });
+    it('successfully loads history page', function() {
+        cy.visit('/history');
+    });
+    it('successfully loads about page', function() {
+        cy.visit('/about');
+    });
 });
